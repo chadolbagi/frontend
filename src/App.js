6aec067f165f06be9c7b8ad7d71ab05583ecb7f6
jeongukjae <JeongUkJae@gmail.com>
2020-06-07 14:09:19 +0900

    add default pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
 import React from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Header from "./components/Header";
+import CallerPage from "./pages/CallerPage";
+import IndexPage from "./pages/IndexPage";
+import OperatorPage from "./pages/OperatorPage";
 import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
+    <Router>
+      <CssBaseline />
+      <Header />
+
+      <Switch>
+        <Route path="/caller">
+          <CallerPage />
+        </Route>
+        <Route path="/operator">
+          <OperatorPage />
+        </Route>
+        <Route path="/">
+          <IndexPage />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ src/pages/OperatorPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OperatorPage = () => {
+  return <>OperatorPage</>;
+};
+
+export default OperatorPage;
